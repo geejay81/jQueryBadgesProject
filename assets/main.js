510819aa422cc7390e743a663c34e94b17f40b01
Greg Parr <gregparr@Gregs-MacBook-Air.local>
2017-11-19 00:25:45 +0000

    Completed project

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/greg81uk.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var completed = response.courses.completed;
+      for(var i = 0; i < completed.length; i++) {
+        var obj = completed[i];
+    
+        $('#badges').append(
+          '<div class="course">' + 
+          '<h3>' + obj['title'] + '</h3>' +
+          '<img src="' + obj['badge'] + '">' +
+          '<a class="btn btn-primary" href="' + obj['url'] + '" target="_blank">See Course</a>' +
+          '</div>'
+        );
+      }
+    }
+  });
 
 });
